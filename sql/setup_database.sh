155e763acfb2412dae213ec8173002c762f61f6a
Raphael Reyna <raphaelreyna@protonmail.com>
2019-06-29 00:15:06 -0700

    Create tables and views before functions while setting up database.

--- sql/setup_database.sh
+++ sql/setup_database.sh
@@ -6,11 +6,11 @@ function execute_all_sql {
 	done
 }
 
+cd ../../tables && execute_all_sql
+cd ../views && execute_all_sql
 cd ./functions/admin && execute_all_sql
 cd ../get && execute_all_sql
 cd ../insert && execute_all_sql
 cd ../state
 cat get_max_degree.sql number_of_roots_logged_for_polynomial.sql | psql lwp_roots
 execute_all_sql
-cd ../../tables && execute_all_sql
-cd ../views && execute_all_sql
